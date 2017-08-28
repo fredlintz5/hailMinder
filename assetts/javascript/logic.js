6a201f28c2bd58eadc488e11c59a87fc33c5ee7d
Fred Lintz <fredlintz@Freds-MacBook-Pro.local>
2017-08-27 23:20:01 -0600

    moved loggedIn js to seperate file, updated profile page layout

---
+++ assetts/javascript/logic.js
@@ -0,0 +1,59 @@
+
+// Initialize Firebase
+var config = {
+  apiKey: "AIzaSyClRKGEi4sOAzyk6pOn8oxIevbY3zOoy7I",
+  authDomain: "hailminder-d07a5.firebaseapp.com",
+  databaseURL: "https://hailminder-d07a5.firebaseio.com",
+  projectId: "hailminder-d07a5",
+  storageBucket: "hailminder-d07a5.appspot.com",
+  messagingSenderId: "150987850599"
+};
+firebase.initializeApp(config);
+    
+
+initApp = function() {
+  firebase.auth().onAuthStateChanged(function(user) {
+    if (user) {
+      // User is signed in.
+      var displayName = user.displayName;
+      var email = user.email;
+      var emailVerified = user.emailVerified;
+      var photoURL = user.photoURL;
+      var uid = user.uid;
+      var phoneNumber = user.phoneNumber;
+      var providerData = user.providerData;
+      user.getIdToken().then(function(accessToken) {
+        $("#displayName").html("Hello " + displayName + " !" )
+        document.getElementById('sign-in-status').textContent = 'You are currently logged in';
+        document.getElementById('sign-in').textContent = 'Sign out';
+        // document.getElementById('account-details').textContent = JSON.stringify({
+        //   displayName: displayName,
+        //   email: email,
+        //   emailVerified: emailVerified,
+        //   phoneNumber: phoneNumber,
+        //   photoURL: photoURL,
+        //   uid: uid,
+        //   accessToken: accessToken,
+        //   providerData: providerData
+        // }, null, '  ');
+        $('#displayName').attr('val', displayName);
+        $('#email').attr('val', email);
+        $('#phoneNumber').attr('val', phoneNumber);
+        $('#homeZip').attr('val', homeZip);
+        $('#workZip').attr('val', workZip);
+      })
+    } else {
+      // User is signed out.
+      $('sign-in-status').textContent = 'You are currently logged out';
+      $('sign-in').textContent = 'Sign in';
+      $('account-details').textContent = 'null';
+    }
+  }, function(error) {
+    console.log(error);
+  });
+};
+
+window.addEventListener('load', function() {
+  initApp()
+});
+   
