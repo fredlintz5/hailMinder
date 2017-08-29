c72b750bdc3273492a745e6f719a176119c52e24
Fred Lintz <fredlintz@Freds-MacBook-Pro.local>
2017-08-28 22:28:32 -0600

    something new

--- assetts/javascript/logic.js
+++ assetts/javascript/logic.js
@@ -74,7 +74,7 @@ function signOut() {
 }
 
 
-var database = firebase.database().ref().child('users/'+uid);
+var database = firebase.database().ref().child("'" + uid + "'");
 
 database.on("value", function(snapshot) {
 
