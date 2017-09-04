8817c233d00d4bca5af13048f7e23d2085012d69
Fred Lintz <fredlintz@Freds-MacBook-Pro.local>
2017-09-04 10:48:30 -0600

    wtf

--- assetts/javascript/logic.js
+++ assetts/javascript/logic.js
@@ -113,6 +113,30 @@ database.on("value", function(snapshot) {
     }
   }
 
+  function alertEmail(hailArray, uid) {
+    for (var i = 0; i < hailArray.length; i++) {
+      if (snapshot.child('users/' + uid + '/homeZip').val() === hailArray[i]) {
+      
+        var name = ('value', snapshot.child('users/' + uid + '/displayName').val());
+        var email = ('value', snapshot.child('users/' + uid + '/email').val());
+        var homeZip = ('value', snapshot.child('users/' + uid + '/homeZip').val());
+
+        console.log(name, email, homeZip);
+        // sendEmail(name, email, homeZip);
+
+        } else if (snapshot.child('users/' + uid + '/workZip').val() === hailArray[i]) {
+
+        var name = ('value', snapshot.child('users/' + uid + '/displayName').val());
+        var email = ('value', snapshot.child('users/' + uid + '/email').val());     
+          var workZip = ('value', snapshot.child('users/' + uid + '/workZip').val());
+
+          console.log(name, email, workZip);
+          // sendEmail(name, email, workZip);
+
+        } 
+    }
+  } 
+
   alertEmail(todayHailArray, "CDKR5qISSOOkrXAnx3qnxDS9Cro2");
 
 })
