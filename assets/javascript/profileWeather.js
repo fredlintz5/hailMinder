1362be48978e473c6528ac7ae81d177bc18219e4
Fred Lintz <fredlintz@Freds-MacBook-Pro.local>
2017-09-09 20:36:47 -0600

    added weather icons for profile page

--- assets/javascript/profileWeather.js
+++ assets/javascript/profileWeather.js
@@ -16,46 +16,48 @@ function currentweather() {
       .done(function(data) {
 
         $("#city").html(data.name);
-        $("#description_weather").html(data.weather[0].description);
-        // $("#weather_image").attr("src", "https://openweathermap.org/img/w/" + data.weather[0].icon + ".png");
+        $("#descriptionWeather").html(data.weather[0].description);
         $("#temperature").html(Math.round(data.main.temp) + "&#176");
 
-        // //Thunderstorm
-        // if(data.weather[0].id>=200 && data.weather[0].id<=232){  $("#currentWeather").css("background","url('https://upload.wikimedia.org/wikipedia/commons/0/0c/Rooymans2000_-_Morning_Lightning_Strike_in_Singapore_(by).jpg')"); }
+        //Thunderstorm
+        if(data.weather[0].id>=200 && data.weather[0].id<=232){  
+            $("#weatherIcon").attr("src", "assets/images/thunderstorm.png");
+        }
 
-        // //Drizzle
-        // else if(data.weather[0].id>=300 && data.weather[0].id<=321){  
-        //   $("#currentWeather").css("background","url('https://s0.geograph.org.uk/geophotos/04/76/64/4766403_e1f206bf.jpg')"); 
-        // }
+        //Drizzle
+        else if(data.weather[0].id>=300 && data.weather[0].id<=321){  
+          $("#weatherIcon").attr("src", "assets/images/drizzle.png");
+        }
 
-        // //Rain
-        // else if(data.weather[0].id>=500 && data.weather[0].id<=531){  
-        //   $("#currentWeather").css("background","url('https://c1.staticflickr.com/1/50/155406169_2bcb8c025f_b.jpg')"); 
-        // }
+        //Rain
+        else if(data.weather[0].id>=500 && data.weather[0].id<=531){  
+          $("#weatherIcon").attr("src", "assets/images/rain.png");
+        }
 
-        // //Snow
-        // else if(data.weather[0].id>=600 && data.weather[0].id<=622){  
-        //   $("#currentWeather").css("background","url('https://res.freestockphotos.biz/pictures/9/9250-snow-covered-trees-pv.jpg')"); }
+        //Snow
+        else if(data.weather[0].id>=600 && data.weather[0].id<=622){  
+          $("#weatherIcon").attr("src", "assets/images/snow.png");
+        }
 
-        // //Atmosphere
-        // else if(data.weather[0].id>=701 && data.weather[0].id<=781){ 
-        //   $("#currentWeather").css("background","url('https://s0.geograph.org.uk/geophotos/04/58/13/4581325_757d0cff.jpg')"); 
-        // }
+        //Atmosphere
+        else if(data.weather[0].id>=701 && data.weather[0].id<=781){ 
+          $("#weatherIcon").attr("src", "assets/images/hazy.png");
+        }
 
-        // //Clear
-        // else if(data.weather[0].id==800){ 
-        //   $("#currentWeather").css("background","url('https://c2.staticflickr.com/2/1298/1340979055_a669e8cc70_b.jpg')"); 
-        // }
+        //Clear
+        else if(data.weather[0].id==800){ 
+          $("#weatherIcon").attr("src", "assets/images/clear.png");
+        }
 
-        // //Clouds
-        // else if(data.weather[0].id>=801 && data.weather[0].id<=804){ 
-        //   $("#currentWeather").css("background","url('https://www.freegreatpicture.com/Max-Pixel/static/photo/1x/Skyporn-Clouds-Skies-Weather-Lookup-Sky-Cloudy-1571791.jpg')"); 
-        // }
+        //Clouds
+        else if(data.weather[0].id>=801 && data.weather[0].id<=804){ 
+          $("#weatherIcon").attr("src", "assets/images/cloudy.png");
+        }
 
-        // //Extreme
-        // else if(data.weather[0].id>=900){ 
-        //   $("#currentWeather").css("background","url('https://upload.wikimedia.org/wikipedia/commons/1/1a/Dszpics1.jpg')"); 
-        // }
+        //Extreme
+        else if(data.weather[0].id>=900){ 
+          $("#weatherIcon").attr("src", "assets/images/hail.png");
+        }
       })
     });
   }
