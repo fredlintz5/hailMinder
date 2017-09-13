0126a573e839fdff55618a5ff62c4e1e4bf3c9ec
cjatkinson <cjatkinson@homeadvisor.com>
2017-09-12 20:49:35 -0600

    welcome modal

--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -153,8 +153,8 @@ database.on("value", function(snapshot) {
       $("#phoneNumberEntry").hide();
       $("#carrierDropdown").hide();
     }
-  //} else {
-    //modal me bro
+  } else {
+      $('#welcomeModal').modal('toggle');
   }
 })
 
